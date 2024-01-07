e261f5990e42097153cce5d52a406c23a7a24621
Murat Sankaya <muratsankaya2025@u.northwestern.edu>
2024-01-07 12:57:02 -0600

    working on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,27 @@
 import "./Navbar.css";
 import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 const Navbar = () => {
   const navigate = useNavigate();
-
-  return <div className="navbar"></div>;
+  return (
+    <div className="navbar">
+      <Button
+        variant="text"
+        color="primary"
+        onClick={() => navigate("/GeneralView")}
+      >
+        General
+      </Button>
+      <Button
+        variant="text"
+        color="primary"
+        onClick={() => navigate("/PersonalizedView")}
+      >
+        Personalized
+      </Button>
+    </div>
+  );
 };
 
 export default Navbar;
