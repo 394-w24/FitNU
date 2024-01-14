be99e65bcbb7feac10c759caca1f36de0aa3ced4
clairepare <clairempare@gmail.com>
2024-01-14 14:23:16 -0600

    weird react errors

--- src/components/PersonalizedView.jsx
+++ src/components/PersonalizedView.jsx
@@ -1,27 +1,39 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
 import "./PersonalizedView.css";
 import Check from "./Check";
-import Cross from "./Cross"
+import Cross from "./Cross";
 import Profile from "./Profile";
-import { calculateMatchingAll, nextProfile } from "./ProfileHandler";
 import CreateProfile from "./CreateProfile";
-import { useAuthState } from "../utilities/firebase";
+import { useAuthState, useDbData } from "../utilities/firebase";
 
 const PersonalizedView = () => {
   const [user] = useAuthState();
-  calculateMatchingAll()
+  const userId = user ? user : null; // Or however you get the user's ID
+  const [userData, error] = userId ? useDbData(`users/${userId}`) : [null, null];
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (userData || error) {
+      setIsLoading(false);
+    }
+  }, [userData, error]);
+
+  if (!user || isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="personalized-view">
-      {/* conditional: if can find user unique id in database, display the matching page */}
-      {user ?
+      {userData ?
         <div>
           <Cross />
           <Profile />
           <Check />
         </div>
-        : <CreateProfile user={user} />}
-
-    </div>);
+        : <CreateProfile user={user} />
+      }
+    </div>
+  );
 };
 
 export default PersonalizedView;
